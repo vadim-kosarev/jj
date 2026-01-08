3ac92889239c15da0feb546c9a39a358a066a4a9
Vadim Kosarev <vadim.kosarev@gmail.com>
2026-01-08 06:31:49 +0300

    Add --database default to init.sh to prevent connection failure due to non-existent default database

--- docker/clickhouse/brightsky01/init.sh
+++ docker/clickhouse/brightsky01/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-clickhouse-client --user clickhouse --password clickhouse -q "CREATE DATABASE IF NOT EXISTS aabb;"
+clickhouse-client --user clickhouse --password clickhouse --database default -q "CREATE DATABASE IF NOT EXISTS aabb;"
