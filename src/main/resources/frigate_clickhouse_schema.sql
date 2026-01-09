9b83dd96d38b21b38cb6067fc8d8b607430eb2eb
Vadim Kosarev <vadim.kosarev@gmail.com>
2026-01-09 11:08:08 +0300

    Update ClickHouse schema: frigate_clickhouse_schema.sql

--- src/main/resources/frigate_clickhouse_schema.sql
+++ src/main/resources/frigate_clickhouse_schema.sql
@@ -1,38 +1,43 @@
+create database if not exists frigate;
+use frigate;
+
 -- frigate.rabbitmq_events definition
 
 CREATE table if not EXISTS frigate.q_frigate_events_mq
 (
-    `message_body` String
+    `message_body` JSON
 )
     ENGINE = RabbitMQ
-        SETTINGS rabbitmq_host_port = 'rabbitmq.rgzz:5672',
-            rabbitmq_username = 'rgzz',
-            rabbitmq_password = 'rgzz',
-            rabbitmq_vhost = 'rgzz',
+        SETTINGS rabbitmq_address = 'amqp://rgzz:rgzz@rabbitmq.rgzz:5672/rgzz',
             rabbitmq_queue_base = 'q-frigate-events',
             rabbitmq_queue_consume = 1,
             rabbitmq_exchange_name = '',
-            rabbitmq_format = 'JSONAsString',
+            rabbitmq_format = 'JSONAsObject',
             rabbitmq_num_consumers = 1,
-            rabbitmq_skip_broken_messages = 1;
+            rabbitmq_skip_broken_messages = 0;
 
 
--- frigate.events_raw definition
-
-CREATE TABLE if not exists frigate.q_frigate_events_raw
+CREATE TABLE IF NOT EXISTS frigate.q_frigate_events_raw
 (
-    `message_body` String,
-    `ingested_at` DateTime DEFAULT now()
+    `message_hash` FixedString(32),
+    `message_body` JSON,
+    `ingested_at`  DateTime DEFAULT now()
 )
-    ENGINE = MergeTree
-        ORDER BY ingested_at
-        SETTINGS index_granularity = 8192;
+    ENGINE = ReplacingMergeTree()
+        ORDER BY (message_hash);
+-- для дедупликации с приоритетом свежей записи
 
-
-create materialized view if not exists frigate.q_frigate_event_mv
+-- MV
+CREATE MATERIALIZED VIEW if not exists frigate.q_frigate_event_mv
             to frigate.q_frigate_events_raw
-as
-select
-    `message_body`
-from frigate.q_frigate_events_mq;
+AS
+SELECT lower(hex(sipHash128(message_body))) AS message_hash, -- или MD5(message_body)
+       message_body,
+       now()                    AS ingested_at
+FROM frigate.q_frigate_events_mq;
+
+OPTIMIZE TABLE frigate.q_frigate_events_raw FINAL;
+
+select * from frigate.q_frigate_events_raw FINAL;
+
 
