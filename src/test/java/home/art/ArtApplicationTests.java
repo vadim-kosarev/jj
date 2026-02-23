ea855ef3fe15f94aa2a46acee3cd5a325299fd6b
Vadim Kosarev <vadim.kosarev@gmail.com>
2026-02-23 23:14:52 +0300

    kafka: fix listeners, add healthcheck, kafka-init topic creation service

--- src/test/java/home/art/ArtApplicationTests.java
+++ src/test/java/home/art/ArtApplicationTests.java
@@ -3,11 +3,25 @@ package home.art;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.List;
+
 class ArtApplicationTests {
+
+	public static class B {
+		B b = new B();
+		public int show() {
+			return 12;
+		}
+	}
+
+
 
 	@Test
 	void contextLoads() {
+
 	}
+
+
 
 }
